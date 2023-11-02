3b1294e6ba6a5c5f35c1c0b19c48559d991358af
Amalrajtpnr <105050258+Amalrajtpnr@users.noreply.github.com>
2023-11-02 14:02:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  output: 'standalone',
+}
